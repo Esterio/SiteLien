79cb590c7b17e5775c2e3252a82d2b664d5c9dcd
Alexander Mist <31696251+Esterio@users.noreply.github.com>
2017-11-15 16:44:22 +0100

    Text input on Chine

---
+++ articleChine.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="Chine.css"> 
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+		<script>tinymce.init({
+				  selector: 'textarea',  // change this value according to your HTML
+				  toolbar: false,
+				  branding: false,
+				  width:"110%",
+				  
+				});</script>
+
+		<title>Voyages autour du monde</title>
+	</head>
+	
+	<body>
+	<?php
+		include 'navbarchine.php';
+	?>
+	<div class="container">
+	<div class="row">
+    <div class="col">
+      <form action="enregistrementarticle.php">
+    <textarea id="mytextarea"></textarea>
+	<input type="submit" value="Submit">
+	</form>
+    </div>
+	
+	</body>
+</html>
