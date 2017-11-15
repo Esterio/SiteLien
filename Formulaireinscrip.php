14dd67076f89220cf04a0bf0d52cf5082aa74eaf
AnaelleT <33691164+AnaelleT@users.noreply.github.com>
2017-11-15 14:20:05 +0100

    Ajout version site anaelle

--- Formulaireinscrip.php
+++ Formulaireinscrip.php
@@ -1,3 +1,5 @@
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 <div class="container">
 			<div class="row main">
 				<div class="panel-heading">
@@ -10,10 +12,10 @@
 					<form class="form-horizontal" method="post" action="#">
 						
 						<div class="form-group">
-							<label for="name" class="cols-sm-2 control-label">Nom</label>
+							<label for="name" class="cols-sm-2 control-label">Nom:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i><i class="glyphicon glyphicon-user"></i></span>
 									<input type="text" class="form-control" name="name" id="name"  placeholder="Smith"/>
 								</div>
 							</div>
@@ -21,10 +23,10 @@
 						
 						
 						<div class="form-group">
-							<label for="username" class="cols-sm-2 control-label">Prénom</label>
+							<label for="username" class="cols-sm-2 control-label">Prénom:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i><i class="glyphicon glyphicon-user"></i></span>
 									<input type="text" class="form-control" name="username" id="username"  placeholder="John"/>
 								</div>
 							</div>
@@ -32,10 +34,10 @@
 						
 						
 						<div class="form-group">
-							<label for="username" class="cols-sm-2 control-label">Date de naissance</label>
+							<label for="username" class="cols-sm-2 control-label">Date de naissance:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i><i class="glyphicon glyphicon-gift"></i></span>
 									<input type="text" class="form-control" name="date" id="date"  placeholder="jj/mm/aaaa"/>
 								</div>
 							</div>
@@ -43,10 +45,10 @@
 						
 						
 						<div class="form-group">
-							<label for="username" class="cols-sm-2 control-label">Ville</label>
+							<label for="username" class="cols-sm-2 control-label">Ville:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i><i class="glyphicon glyphicon-home"></i></span>
 									<input type="text" class="form-control" name="username" id="username"  placeholder="Paris"/>
 								</div>
 							</div>
@@ -54,10 +56,10 @@
 						
 						
 						<div class="form-group">
-							<label for="username" class="cols-sm-2 control-label">Pays</label>
+							<label for="username" class="cols-sm-2 control-label">Pays:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i><i class="glyphicon glyphicon-globe"></i></span>
 									<input type="text" class="form-control" name="username" id="username"  placeholder="France"/>
 								</div>
 							</div>
@@ -65,10 +67,10 @@
 						
 						
 						<div class="form-group">
-							<label for="email" class="cols-sm-2 control-label">E-mail</label>
+							<label for="email" class="cols-sm-2 control-label">E-mail:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i><i class="glyphicon glyphicon-envelope"></i></span>
 									<input type="text" class="form-control" name="email" id="email"  placeholder="John.Smith@gmail.com"/>
 								</div>
 							</div>
@@ -76,10 +78,10 @@
 
 						
 						<div class="form-group">
-							<label for="password" class="cols-sm-2 control-label">Mot de passe</label>
+							<label for="password" class="cols-sm-2 control-label">Mot de passe:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i><i class="glyphicon glyphicon-lock"></i></span>
 									<input type="password" class="form-control" name="password" id="password"/>
 								</div>
 							</div>
@@ -87,10 +89,10 @@
 
 						
 						<div class="form-group">
-							<label for="confirm" class="cols-sm-2 control-label">Confirmation du mot de passe</label>
+							<label for="confirm" class="cols-sm-2 control-label">Confirmation du mot de passe:</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i><i class="glyphicon glyphicon-lock"></i></span>
 									<input type="password" class="form-control" name="confirm" id="confirm"/>
 								</div>
 							</div>
@@ -100,6 +102,20 @@
 						<div class="form-group ">
 							<button type="button" class="btn btn-primary btn-lg btn-block login-button">S'inscrire</button>
 						</div>
+						    </div>
+						
+<div class="container">
+    <hr>
+        <div class="text-center center-block">
+		
+                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+</div>
+    <hr>
+</div>  
+
+	
 						<div class="login-register">
 				            <a href="Se_connecter.php">Se connecter</a>
 				         </div>
